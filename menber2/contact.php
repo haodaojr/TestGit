104693d43bb417f6381dd360b4b7c85c9151f0b2
haodaojr <haodnph53058@gmail.com>
2025-06-06 14:56:21 +0700

    thêm 2 file mới để thử trong menber-menbermenber2

---
+++ menber2/contact.php
@@ -0,0 +1,9 @@
+<?php
+// public function __construct(Type $var = null) {
+//     $this->load = $var;
+// }
+class Contact {
+    public function index() {
+        $this->load->view('contact');
+    }
+}
---
+++ menber2/index.php
@@ -0,0 +1,4 @@
+<?php 
+echo "<h1>Menber2</h1>";
+echo "<h2>Menber2</h2>";
+?>
